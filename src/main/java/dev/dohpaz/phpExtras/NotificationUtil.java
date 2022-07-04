c1a28966632f8de5a39c7d054ecc0d5fc9d7775c
Ken Stanley <dohpaz@gmail.com>
2022-07-04 14:22:58 -0400

    Use non-deprecated method

--- src/main/java/dev/dohpaz/phpExtras/NotificationUtil.java
+++ src/main/java/dev/dohpaz/phpExtras/NotificationUtil.java
@@ -24,7 +24,7 @@ public class NotificationUtil {
             return;
         }
 
-        Notification notification = group.createNotification("Module include path removed", module + ": " + message, notificationType, null);
+        Notification notification = group.createNotification("Module include path removed", module + ": " + message, notificationType);
         ApplicationManager.getApplication().invokeLater(() -> {
             Notifications.Bus.notify(notification, project);
         });
