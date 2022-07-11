6c52e051ad4eb88e8a446e67ed183884a6a2a646
Ken Stanley <dohpaz@gmail.com>
2022-07-11 14:52:09 -0400

    Rename insection to better describe its purpose

--- src/main/java/dev/dohpaz/phpExtras/php/codeInspection/PhpComposerAutoloadDevPackageToContentRootInspection.java
+++ src/main/java/dev/dohpaz/phpExtras/php/codeInspection/PhpComposerAutoloadDevPackageToContentRootInspection.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class ComposerPackageToModuleInspection extends PhpInspection {
+public class PhpComposerAutoloadDevPackageToContentRootInspection extends PhpInspection {
     final LocalFileSystem localFileSystem = LocalFileSystem.getInstance();
 
     @NotNull
